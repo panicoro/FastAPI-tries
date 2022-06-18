c21a64d98856d590f13200421c6b6c37e486b3d7
Pablo Rosa <panicoro770@gmail.com>
2022-06-18 20:25:57 -0300

    Adding query params

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from enum import Enum
 
 app = FastAPI()
@@ -8,6 +8,10 @@ class UserType(str, Enum):
     STANDARD = "standard"
     ADMIN = "admin"
 
+
+class UsersFormat(str, Enum):
+    SHORT = "short"
+    FULL = "full"
 
 @app.get("/")
 async def hello_world():
@@ -20,7 +24,7 @@ async def get_user_by_type(user_type: UserType, id_user: int):
 
 
 @app.get("/users/{id_user}")
-async def get_user(id_user: int = Path(..., ge=1)):
+async def get_user_by_id(id_user: int = Path(..., ge=1)):
     return {"id": id_user}
 
 
@@ -30,6 +34,11 @@ async def get_license_plate(lic: str = Path(..., min_length=9, max_length=9,
     return {"license": lic}
 
 
-@app.get("/users")
-async def get_user(page: int = 1, size: int = 10):
+@app.get("/users-pages")
+async def get_users(page: int = Query(1, gt=0), size: int = Query(10, le=100)):
     return {"page": page, "size": size}
+
+
+@app.get("/all-users")
+async def get_all_user(user_format: UsersFormat):
+    return {"format": user_format}
